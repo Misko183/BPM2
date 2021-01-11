3ff38c4ccc6f9e885c468133e4185aa11d090566
misko183 <rtmike183@gmail.com>
2021-01-11 20:41:03 +0100

    MVVM, ViewModel, View, Model,

--- BPM2/BPM2/App.xaml.cs
+++ BPM2/BPM2/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BPM2.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
---
+++ BPM2/BPM2/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace BPM2.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent(); 
+        }
+    }
+}
